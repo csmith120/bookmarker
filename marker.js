a2217c6297ad22c1a7aa252ff8c7af2e808dc262
CSmith3s <CSmith3s@capetigers.com>
2024-04-16 09:50:51 -0500

    streemline magic

--- marker.js
+++ marker.js
@@ -22,20 +22,58 @@ overlay.addEventListener('click', closeFloater);
 const bookmarksList = document.querySelector('.bookmarks-list');
 const bookmarkForm = document.querySelector('.bookmark-form');
 const bookmarkInput = bookmarkForm.querySelector('input[type]');
+const bookmarks =[];
 
 function createBookmark(e) {
     e.preventDefault();
 
+    //add a new bookmark to bookmark
     const title = bookmarkInput.value;
-    const bookmark = document.createElement('a');
+    const bookmark ={
+        title: title
+    };
 
-    bookmark.className = 'bookmark';
-    bookmark.innerText = title;
-    bookmark.href = '#';
-    bookmark.target = '_blank';
-    bookmarksList.appendChild(bookmark);
-
+    bookmarks.push(bookmark);
+    fillBookmarksList(bookmarks);
+    storeBookmarks(bookmarks);
     bookmarkForm.reset();
+    // save that bookmark to local storage
+
+    //const title = bookmarkInput.value;
+    //const bookmark = document.createElement('a');
+
+    //bookmark.className = 'bookmark';
+    //bookmark.innerText = title;
+    //bookmark.href = '#';
+    //bookmark.target = '_blank';
+    //bookmarksList.appendChild(bookmark);
+}
+
+function fillBookmarksList(bookmarks = []) {
+
+    const bookmarksHtml = bookmarks.map((bookmark) => {
+        return `
+        <a href="#" class="bookmark">
+        ${bookmarks[i].title}
+        </a>
+        `;
+    }).join('');
+
+    bookmarksList.innerHTML = bookmarksHtml;
+
+ //let bookmarksHtml = '';
+ //for (let i = 0; i < bookmarks.length; i++) {
+    //bookmarksHtml +=`
+   //<a href="#" class="bookmark">
+   // ${bookmarks[i].title}
+    //</a>
+   // `;
+ //}
+
+}
+
+function storeBookmarks(bookmarks = []) {
+    localStorage.setItem('bookmarks', bookmarks);
 }
 
 bookmarkForm.addEventListener('submit', createBookmark);
